a244e1ed938240abf0c326af1cd49354ca0bce43
“Owusu <owusuachamfour@gmail.com>
2023-09-25 18:52:56 -0400

    fruits...

--- views/fruits/Index.jsx
+++ views/fruits/Index.jsx
@@ -1,35 +1,5 @@
-/*const React = require("react");
-
-class Index extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Fruits index page</h1>
-        <ul>
-          {this.props.fruits.map((fruit, i) => {
-            return (
-              <li>
-                The <a href={`/fruits/${fruit.id}`}> {fruit.name} </a> is
-                {fruit.color}
-                {fruit.readyToEat
-                  ? `It is ready to eat`
-                  : `It is not ready to eat`}
-              </li>
-            );
-          })}
-        </ul>
-        <nav>
-          <a href="/fruits/new">Create a New Fruit</a>
-        </nav>
-      </div>
-    );
-  }
-}
-
-module.exports = Index;
-*/
 const React = require("react");
-const DefaultLayout = require("./layouts/default");
+const DefaultLayout = require("./layouts/Default.jsx");
 
 class Index extends React.Component {
   render() {
